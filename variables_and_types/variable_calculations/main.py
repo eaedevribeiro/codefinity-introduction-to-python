b648c82a4477a8d06a73df317daee35d5797b8a6
eaedevribeiro <>
2026-08-04 14:49:50 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,7 @@
+item_name= "Soda"
+item_price= 6.99
+purchase_quantity= 5
+#calcule total cost before printing
+total_cost = item_price*purchase_quantity
+#display the result
+print("total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
