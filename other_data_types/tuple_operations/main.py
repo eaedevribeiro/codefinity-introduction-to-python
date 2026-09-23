07a82569eced6567639b88025a43e620f37367fb
eaedevribeiro <>
2026-09-23 19:13:27 +0000

    Tuple Operations

--- other_data_types/tuple_operations/main.py
+++ other_data_types/tuple_operations/main.py
@@ -4,8 +4,8 @@ shelf1_update = ["tomatoes", "celery", "cilantro"]
 shelf1_update_tuple = tuple(shelf1_update)
 shelf1_concat = shelf1 + shelf1_update_tuple
 celery_count = shelf1_concat.count("celery")
-celery_count = shelf1_concat.index ("celery")
+celery_index = shelf1_concat.index("celery")
 # Items being added to the shelf #1 (provided as a list)
 print("Updated Shelf #1:", shelf1_concat)
 print("Number of Celery:", celery_count)
-print("Celery Index")
+print("Celery Index:", celery_index)
